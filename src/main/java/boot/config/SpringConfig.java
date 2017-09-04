cbfc8c9375b77d4c57d3374ccfb12bdbbc11c88e
huishen <darkaquarius88@163.com>
2017-09-04 11:53:36 +0800

    HeaderHttpSessionStrategy or CookieHttpSessionStrategy

--- src/main/java/boot/config/SpringConfig.java
+++ src/main/java/boot/config/SpringConfig.java
@@ -40,7 +40,6 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
-import org.springframework.session.web.http.HeaderHttpSessionStrategy;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.thymeleaf.spring4.SpringTemplateEngine;
@@ -337,10 +336,10 @@ public class SpringConfig {
     //     return rmiProxy;
     // }
 
-    // 注册filter
-    @Bean
-    public HeaderHttpSessionStrategy headerHttpSessionStrategy() {
-        return new HeaderHttpSessionStrategy();
-    }
+    // // 使用HTTP header将请求与session关联
+    // @Bean
+    // public HeaderHttpSessionStrategy headerHttpSessionStrategy() {
+    //     return new HeaderHttpSessionStrategy();
+    // }
 
 }
